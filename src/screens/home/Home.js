8ce45041a9885e68a82577d5c2db464764f4a605
joelvarghesejoy <joel.v@chaipoint.com>
2018-10-21 12:44:49 +0530

    Added released movie part

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -22,12 +22,22 @@ const styles = theme => ({
        flexWrap: 'nowrap',
        transform: 'translateZ(0)',
        width: '100%',
+   },
+   gridListReleasedMovies:{
+     transform: 'translateZ(0)',
+     cursor: 'pointer'
    }
 });
 
 class Home extends Component{
+
+  getFormattedReleaseDate = (date) => {
+    return new Date(date);
+  }
+
   render(){
     const { classes } = this.props;
+    const that = this;
     return(
       <div>
         <Header></Header>
@@ -46,6 +56,25 @@ class Home extends Component{
             })
           }
         </GridList>
+        <div className="flex-container">
+          <div className="left">
+            <GridList cellHeight={350} cols={4} className={classes.gridListReleasedMovies}>
+              {
+                MovieData.map(function(movie){
+                  return(
+                    <GridListTile className="released-movie-grid-item" key={"grid"+movie.id}>
+                      <img className="movie-release-poster" src={movie.poster_url} alt={movie.title}/>
+                      <GridListTileBar title={movie.title} subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}/>
+                    </GridListTile>
+                  );
+                })
+              }
+            </GridList>
+          </div>
+          <div className="right">
+
+          </div>
+        </div>
       </div>
     );
   }
