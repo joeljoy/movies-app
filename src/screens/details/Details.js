c9d7f35bbac1c4472b471731691e058110b44e69
joelvarghesejoy <joel.v@chaipoint.com>
2018-10-21 16:48:09 +0530

    added artist setcion

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -6,6 +6,9 @@ import './Details.css';
 import YouTube from 'react-youtube';
 import Home from '../home/Home';
 import ReactDOM from 'react-dom';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 class Details extends Component {
   constructor() {
@@ -14,6 +17,7 @@ class Details extends Component {
       movie: {}
     }
   }
+
   componentWillMount() {
     let currentState = this.state;
     currentState.movie = moviesData.filter((mov) => {
@@ -22,26 +26,30 @@ class Details extends Component {
     this.setState({currentState});
   }
 
+  artistClickHandler = (url) => {
+    window.location = url;
+  }
+
   backToHomeHandler = () => {
-        ReactDOM.render(<Home />, document.getElementById('root'));
-    }
+    ReactDOM.render(<Home/>, document.getElementById('root'));
+  }
 
   render() {
     let movie = this.state.movie;
     const opts = {
-            height: '300',
-            width: '700',
-            playerVars: {
-                autoplay: 1
-            }
-        }
+      height: '300',
+      width: '700',
+      playerVars: {
+        autoplay: 1
+      }
+    }
     return (<div className="details">
       <Header/>
       <div className="back">
-                    <Typography onClick={this.backToHomeHandler}>
-                        &#60; Back to Home
-                        </Typography>
-                </div>
+        <Typography onClick={this.backToHomeHandler}>
+          &#60; Back to Home
+        </Typography>
+      </div>
       <div className="flex-containerDetails">
         <div className="leftDetails">
           <img src={movie.poster_url} alt={movie.title}/>
@@ -85,18 +93,30 @@ class Details extends Component {
               </Typography>
             </div>
             <div className="trailerContainer">
-                            <Typography>
-                                <span className="bold">Trailer:</span>
-                            </Typography>
-                            <YouTube
-                                videoId={movie.trailer_url.split("?v=")[1]}
-                                opts={opts}
-                                onReady={this._onReady}
-                            />
-                        </div>
+              <Typography>
+                <span className="bold">Trailer:</span>
+              </Typography>
+              <YouTube videoId={movie.trailer_url.split("?v=")[1]} opts={opts} onReady={this._onReady}/>
+            </div>
           </div>
         </div>
-        <div className="rightDetails"></div>
+        <div className="rightDetails">
+          <div className="bold marginBottom16 marginTop16">
+            <Typography>
+              <span className="bold">Artists:</span>
+            </Typography>
+          </div>
+          <div className="paddingRight">
+            <GridList cellHeight={160} cols={2}>
+              {
+                movie.artists != null && movie.artists.map(artist => (<GridListTile className="gridTile" onClick={() => this.artistClickHandler(artist.wiki_url)} key={artist.id}>
+                  <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name}/>
+                  <GridListTileBar title={artist.first_name + " " + artist.last_name}/>
+                </GridListTile>))
+              }
+            </GridList>
+          </div>
+        </div>
       </div>
     </div>)
   }
